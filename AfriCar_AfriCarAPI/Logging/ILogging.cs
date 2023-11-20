862ba96e5c7cb1547750b4e0599ea5c28bb8f1a9
Itumeleng Kekana <itumeleng@letswhoosh.com>
2023-11-20 17:13:08 +0200

    Added Logging files.

---
+++ AfriCar_AfriCarAPI/Logging/ILogging.cs
@@ -0,0 +1,7 @@
+﻿namespace AfriCar_AfriCarAPI.Logging
+{
+	public interface ILogging
+	{
+		public void Log(string message, string type);
+	}
+}
---
+++ AfriCar_AfriCarAPI/Logging/LoggingV2.cs
@@ -0,0 +1,30 @@
+﻿namespace AfriCar_AfriCarAPI.Logging
+{
+	public class LoggingV2 : ILogging
+	{
+		public void Log(string message, string type)
+		{
+			if (type == "error")
+			{
+				Console.BackgroundColor = ConsoleColor.Red;
+				Console.WriteLine("ERROR - " + message);
+				Console.BackgroundColor = ConsoleColor.Black;
+
+			}
+			else
+			{
+				if (type == "warning")
+				{
+					Console.BackgroundColor = ConsoleColor.DarkYellow;
+					Console.WriteLine("WARINING - " + message);
+					Console.BackgroundColor = ConsoleColor.Black;
+
+				}
+				else
+				{
+					Console.WriteLine(message);
+				}
+			}
+		}
+	}
+}
