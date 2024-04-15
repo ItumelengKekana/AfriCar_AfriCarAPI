e0ae3aa03ae30e17e359dfbd813b31bc233c7e01
kekanaitumeleng7@gmail.com <kekanaitumeleng7@gmail.com>
2024-04-15 17:02:08 +0200

    First part of consuming secured API.

---
+++ AfriCar_Web/Models/Dto/RegistrationRequestDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace AfriCar_Web.Models.Dto
+{
+	public class RegistrationRequestDTO
+	{
+		public string UserName { get; set; }
+		public string Name { get; set; }
+		public string Password { get; set; }
+		public string Role { get; set; }
+	}
+}
